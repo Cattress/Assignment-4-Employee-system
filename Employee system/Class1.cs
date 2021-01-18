7b85fd9144dbd3fd054f4a3036be416b4565033a
Cattress <priyaphon.vincent@gmail.com>
2021-01-18 12:21:19 +0700

    Add project files.

---
+++ Employee system/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_system
+{
+    class Employee
+    {
+        private string name;
+        private int age;
+        private DateTime dob;
+        private string job;
+        private double salary;
+
+        private static int employeeCount();
+        private static int retirementAge = 65;
+
+        public Employee(string -name, DateTime -dob, string -job, double -salary)
+        {
+            name = -name;
+            job = -job;
+            salary = -salary;
+            dob = -dob;
+
+            //employeeCount = employeeCount+1
+            //employeeCount += 1;       same as above
+            employeeCount++; //same as above
+
+        }
+        public double GetMonthlySalary()
+        {
+            double MonthlySalary = salary / 12;
+            return MonthlySalary;
+        }
+
+        public bool isBirthday()
+        {
+            int month = dob.Month;
+            int day = dob.Day;
+
+            if (month == DateTime.Now.Month && day == DateTime.Now.Day)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void SetAge()
+        {        
+                            // 2021 - 2004 = 17
+            age = DateTime.Now.Year - dob.Year;
+        }
+
+        public int GetYearsUntilRetirement()
+        {
+            SetAge();
+                                                //65 - 17
+            int YearsUntilRetirement = retirementAge - age;
+            return YearsUntilRetirement;
+        }
+        public string GetInformation()
+        {
+            string information = "Name: "+name +" Age: " +age + " Job: "+job + " Salary: "+salary;
+
+        }
+    }
+}
---
+++ Employee system/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Employee_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Toy = new Employee("Toy?", new DateTime(2004, 3, 13),"Student",13);
+            Console.WriteLine(Employee.GetNumOfEmployees());
+            Toy.SetJob("Plumber");
+            Toy.SetSalary(6);
+            Toy.SetName("Mrs Toy");
+            Console.WriteLine(Toy.GetMonthlySalary());
+            Console.WriteLine(Toy.isBirthday());
+            Console.WriteLine(Toy.GetYearsUntilRetirement());
+            Console.WriteLine(Toy.GetInformation());
+
+            Employee Ankit = new Employee("Ankit", new DateTime(2004, 10, 25), "Pilot", 1231);
+            Console.WriteLine(Employee.employeeCount);
+        }
+    }
+}
